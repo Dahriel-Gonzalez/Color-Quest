6739ba977bf7e29614b6373064ace58831f37ead
Dahriel-Gonzalez <99700458+Dahriel-Gonzalez@users.noreply.github.com>
2022-12-09 13:47:33 -0500

    commented out broken lines

--- Assets/Scripts/nextTrigger.cs
+++ Assets/Scripts/nextTrigger.cs
@@ -6,18 +6,18 @@ using UnityEngine.SceneManagement;
 public class nextTrigger : MonoBehaviour
 {
     public string sceneName;
-    [SerializeField] private levelController lvlCtr;
+    //[SerializeField] private levelController lvlCtr;
 
     void Start()
     {
-        lvlCtr = GameObject.FindGameObjectsWithTag("levelController")[0].GetComponent<levelController>();
+        //lvlCtr = GameObject.FindGameObjectsWithTag("levelController")[0].GetComponent<levelController>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            lvlCtr.unlockLevel(sceneName);
+            //lvlCtr.unlockLevel(sceneName);
             SceneManager.LoadScene(sceneName);
         }
     }
